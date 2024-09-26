5b3ac050986253b10b228d3c626a5d8d95d71602
Kevinbourdal <kevin.bourdal@outlook.com>
2024-09-26 10:07:45 -0300

    Fix(setup)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = "0.0.12"
+VERSION = "0.0.13"
 PACKAGE_NAME = "quick_rabbit"
 AUTHOR = "Kevin Lopez Bourdal"
 AUTHOR_EMAIL = "kevin.bourdal@outlook.com"
